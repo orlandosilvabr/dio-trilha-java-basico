9202cfc62c74ff719e496cc0ecd0fe0eb19ac98b
Orlando <orlandosilvabr@yahoo.com.br>
2024-01-05 13:05:55 -0300

    Versão 1 completa

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,5 +1,23 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner teclado = new Scanner(System.in);
+        int numero;
+        String agencia;
+        String nome;
+        double saldo;
+
+        System.out.print("Digite o número da conta: ");
+        numero  = teclado.nextInt();
+        System.out.print("Digite o número da agência: ");
+        agencia = teclado.nextLine(); teclado.nextLine();
+        System.out.print("Digite o nome: ");
+        nome = teclado.nextLine();
+        System.out.print("Digite o saldo da conta: ");
+        saldo = teclado.nextDouble();
+
+        System.out.println("Olá, " + nome + "! Obrigado por criar uma conta em nosso banco, sua agência é a " + agencia + 
+            ", conta " + numero + " e seu saldo de " + saldo + " já está disponível.");
     }
 }
